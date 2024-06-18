cb54e54a832a55914c0dfdd4b5b9397106b7f4c8
SardanaTeam <lexoprod@gmail.com>
2024-06-18 17:29:46 -0300

    Initial commit

---
+++ src/screens/LandingPage/LandingPage.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import "./style.css";
+
+export const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <img className="designer" alt="Designer" src="/img/designer-39-4.png" />
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
+          <p className="website-under">
+            Website Under development
+            <br />
+            Follow our socials for updates!
+            <br />
+            x.com/sardanacoin
+            <br />
+            t.me/sardanacoin
+          </p>
+          <img className="img" alt="Rectangle" src="/img/rectangle-2.png" />
+          <div className="text-wrapper">sardana.io</div>
+          <img className="designer-2" alt="Designer" src="/img/designer-29-1.png" />
+          <img className="pie-chart-no-bg" alt="Pie chart no bg" src="/img/pie-chart-no-bg-1.png" />
+          <img className="designer-3" alt="Designer" src="/img/designer-39-3.png" />
+          <div className="text-wrapper-2">Sardana</div>
+          <img className="sardana" alt="Sardana" src="/img/sardana-6.png" />
+          <p className="no-tax-mint-revoked">
+            no tax!
+            <br />
+            mint revoked!
+            <br />
+            lp token burn!
+            <br />
+            development and
+            <br />
+            marketing tokens locked!
+          </p>
+          <div className="text-wrapper-3">Coming Soon!</div>
+          <div className="text-wrapper-4">Meet Sardana</div>
+          <p className="in-the-vast-digital">
+            In the vast digital ocean of blockchain, sardines swam peacefully until giant whales began to dominate,
+            devouring the smaller fish with their immense resources. Tired of being prey, a brave group of sardines, led
+            by Sardana, decided to take a stand.
+            <br />
+            Harnessing the power of Solana&#39;s network, these sardines created their own economy with Sardana, a
+            decentralized cryptocurrency. Using smart contracts and advanced algorithms, they built a secure and
+            transparent network where every sardine had an equal voice.
+            <br />
+            As news of their resistance spread, other marine communities took notice. Whales, once confident, began to
+            worry as sardines organized and grew stronger.
+            <br />
+            Developers tirelessly built a robust digital infrastructure to support the sardines. Sardana led awareness
+            campaigns, inspiring other marine communities to join their cause against the whales&#39; oppression.
+            <br />
+            As the launch of Sardana approaches, excitement grows... United and ready, the sardines prepare to claim
+            their place in the digital ocean, marking the start of a new era of resistance and empowerment for all small
+            fish daring to challenge the whales!
+          </p>
+          <p className="p">The Tale of Sardana: King of the Sardines</p>
+          <p className="allocation-the">
+            Allocation
+            <br />
+            The allocation amount will be based on the total token sale in a unique model, with the percentages
+            specified below:
+            <br />
+            1%&nbsp;&nbsp;- Promotional Airdrops
+            <br />
+            8% - Reserves
+            <br />
+            8%&nbsp;&nbsp;- Developers
+            <br />
+            10%&nbsp;&nbsp;- Marketing
+            <br />
+            30% - Liquidity
+            <br />
+            43% - Pre Sale
+          </p>
+          <p className="text-wrapper-5">Free airdrops and ICO soon!</p>
+        </div>
+        <div className="overlap">
+          <img
+            className="DALLE"
+            alt="Dalle"
+            src="/img/dall-e-2024-06-02-23-04-14-a-whimsical-depiction-of-sardana-t.png"
+          />
+          <div className="rectangle-4" />
+          <div className="text-wrapper-6">@TEAM SARDANA 2024</div>
+          <div className="text-wrapper-7">t.me/sardanacoin</div>
+          <a className="text-wrapper-8" href="https://t.me/sardanacoin" rel="noopener noreferrer" target="_blank">
+            x.com/sardanacoin
+          </a>
+          <div className="text-wrapper-9">social links:</div>
+          <img className="sardana-2" alt="Sardana" src="/img/sardana-4.png" />
+        </div>
+        <p className="RISK-WARNING-trading">
+          RISK WARNING: Trading Cryptocurrencies is highly speculative, carries a level of risk and may not be suitable
+          for all investors. You may lose some or all of your invested capital, therefore you should not speculate with
+          capital that you cannot afford to lose. The content on this site should not be considered investment advice.
+          Investing is speculative. When investing your capital is at risk.
+          <br />
+          We do not allow users of these countries to participate in the presale: The information on this site is not
+          intended for residents of Afghanistan, Benin, Bhutan, China, Crimea region, Cuba, Iran, Iraq, Syria, USA,
+          Vatican City, or for use by any person in any country or jurisdiction where such distribution or use would be
+          contrary to local law or regulation.
+        </p>
+      </div>
+    </div>
+  );
+};
