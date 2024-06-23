8b1398ed2f29712c5b26161dcc51e43592b3500c
SardanaTeam <lexoprod@gmail.com>
2024-06-23 02:29:13 -0300

    icon add

--- src/screens/LandingPage/LandingPage.jsx
+++ src/screens/LandingPage/LandingPage.jsx
@@ -10,9 +10,9 @@ const LandingPage = () => {
         <meta name="description" content="Sardana is coming soon! Stay tuned for updates on our decentralized cryptocurrency." />
         <meta name="keywords" content="Sardana, Sardana Coin, Sardana.io, coming soon, release, sardanacoin, Cryptocurrency, Blockchain, Decentralized" />
         <meta name="viewport" content="width=device-width, initial-scale=0.2, user-scalable=yes" />
-        <link rel="icon" type="image/png" href="/static/img/sardana-4.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/static/img/sardana-4.png" sizes="16x16" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/static/img/sardana-4.png" />
+        <link rel="icon" type="image/png" href="/img/sardana-4.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="img/sardana-4.png" sizes="16x16" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/sardana-4.png" />
       </Helmet>
       <div className="div">
         <div className="overlap-group">
